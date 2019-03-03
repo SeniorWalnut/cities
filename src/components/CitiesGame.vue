833222b47734f464057d9ede5ff900eaee1d888b
SeniorWalnut <agafonov@etm-system.ru>
2019-03-03 17:43:00 +0200

    added speech recognition

--- src/components/CitiesGame.vue
+++ src/components/CitiesGame.vue
@@ -3,7 +3,7 @@
 		<div class="Cities-game__wrapper">
 			<div class="Cities-game__title">Cities</div>
 			<div v-if="stage === 1" class="Cities-game__stage-one">
-				<button class="Cities-game__start-button" @click="isPlayersName ? startGame() : toPlayerName()" >{{isPlayersName ? 'Enter Players Names:' : 'Input the quantity:'}}</button>
+				<button class="Cities-game__main-button" @click="isPlayersName ? startGame() : toPlayerName()" >{{isPlayersName ? 'Enter Players Names:' : 'Input the quantity:'}}</button>
 				<transition name="fadeRight">
 				<input 
 					v-if="!isPlayersName"
@@ -18,38 +18,44 @@
 					>
 				</transition>
 				<div class="Cities-game__not-enough" v-if="unableToStart">Not enough players to play!</div>
-				<div class="Cities-game__not-enough" v-if="unablePlayersInput">Input something!</div>
+				<div class="Cities-game__not-enough" v-if="unablePlayersInput">The length of the name must be higher than 2 and less than 15!</div>
+
 			</div>
 
-			<transition name="fadeRight">
-				<div v-if="players.length === playersQuan" class="Cities-game__stage-two">
-					<!-- <transition name="fade"></transition> -->
-					<div class="Cities-game__current-player">
-						{{curPlayer}}
-					</div>
-					<!-- <transition name="fadeRight"> -->
-						<input 
-							v-if="useMicro"
-							class="Cities-game__micro-input"
-						/>
-						<div v-else="useMicro" class="Cities-game__without-micro">	
-							<input 
-								v-model="city"
-								class="Cities-game__main-input" 
-							/>
-							<button @click="handleCity">Ok</button>
+			<div v-if="players.length === playersQuan" class="Cities-game__stage-two">
+				<transition name="fadeRight">
+					<div class="Cities-game__transition-wrapper">
+						<div class="Cities-game__current-player">
+							{{curPlayer}}
 						</div>
-					<!-- </transition> -->
-					<div class="Cities-game__city-fact"></div>
+							<button 
+								v-if="useMicro"
+								class="Cities-game__main-button"
+								@click="handleCity"
+							>Tap to speak!</button>
+							<div v-else="useMicro" class="Cities-game__without-micro">	
+								<input 
+									v-model="city"
+									class="Cities-game__main-input" 
+								/>
+								<button 
+									@click="handleCity">Check</button>
+							</div>
+						<!-- </transition> -->
+						<div v-if="curCityName.length" class="Cities-game__city-name">{{curCityName}}</div>	
+						<div v-if="curCityFact.length" class="Cities-game__city-fact">{{curCityFact}}</div>
+						<div class="Cities-game__not-enough" v-if="inputAnotherWarn">Type another city!</div>
+					</div>
+				</transition>
+				<div class="Cities-game__city-count">{{cityCount}}</div>
+				<div class="Cities-game__use-micro">
+					Do you want to use micro?
+					<div class="Cities-game__switch-micro">
+						<div :class="{active: useMicro}" @click="useMicro = true">Yes</div>
+						<div :class="{active: !useMicro}" @click="useMicro = false">No</div>
+					</div>
 				</div>
-			</transition>
-					<div class="Cities-game__use-micro">
-						Do you want to use micro?
-						<div class="Cities-game__switch-micro">
-							<div :class="{active: useMicro}" @click="useMicro = true">Yes</div>
-							<div :class="{active: !useMicro}" @click="useMicro = false">No</div>
-						</div>
-					</div>
+			</div>
 			<div v-if="stage == 3" class="Cities-game__stage-three"></div>
 		</div>
 	</div>
@@ -59,15 +65,33 @@
 import axios from 'axios'
 import data from './cities'
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
+
+const grammar = '#JSGF V1.0; grammar citites; public <city> = ' + data.cities.join(' | ').toLowerCase() + ' ;'
+
+const recognition = new SpeechRecognition()
+const speechRecognitionList = new SpeechGrammarList()
+
+speechRecognitionList.addFromString(grammar, 1)
+
+recognition.grammars = speechRecognitionList
+recognition.lang = 'ru-RU'
+recognition.interimResults = false 
+recognition.maxAlternatives = 1
+
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
 
 export default {
 	name: 'cities-game',
 	data() {
 		return {
 			city: '',
+			maxPlayerTries: 3,
 			isPlayersName: false,
 			playersQuan: 2,
 			cityFact: '',
@@ -77,19 +101,18 @@ export default {
 			curPlayer: '',
 			players: [],
 			useMicro: true,
-			playerTurn: 1
+			playerTurn: 0,
+			usedCities: [],
+			playerTries: this.maxPlayerTries,
+			inputAnotherWarn: false,
+			curCityFact: '',
+			curCityName: '',
+			cityCount: 0
 		}
 	},
 	methods: {
-		getSpeech() {
-			let rec = new SpeechRecognition()
-			let speechRecList = new SpeechGrammarList()
-			speechRecList.addFromString()
-			rec.lang = 'ru-RU'
-
-		},
 		startGame() {
-			if (this.curPlayer.length) {
+			if (this.curPlayer.length > 2 && this.curPlayer.length < 15) {
 				this.players.push(this.curPlayer)
 				this.curPlayer = ''
 				this.unablePlayersInput = false
@@ -110,13 +133,57 @@ export default {
 			}
 		},
 
-		handleCity() {
-			if (this.city.length) {
-				if (data.cities.includes(capitalizeFirstLetter(this.city))) {
+		moveToNext(city) {
+			this.inputAnotherWarn = false
+			this.playerTries = this.maxPlayerTries
+			if (data.cities.includes(city)) {
+				if (!this.usedCities.includes(city)) {
+					this.getCity(city)
 					this.playerTurn = (this.playerTurn == this.playersQuan - 1 ? 0 : ++this.playerTurn)
 					this.curPlayer = this.players[this.playerTurn]
+					this.usedCities.push(city)
+					this.cityCount++
+				} else {
+					if (this.playerTries) this.playerTries-- 
+					else {
+						this.stage = 3
+					}
+				}	
+			} else {
+				this.inputAnotherWarn = true
+			}
+		},
+
+		handleCity() {
+			if (!this.useMicro && this.city.length) 
+				this.moveToNext(this.city)
+			else if (this.useMicro) {
+				recognition.start()
+
+				recognition.onresult = (event) => {
+					const last = event.results.length - 1
+					let trans = event.results[last][0].transcript
+					console.log(trans)
+					this.moveToNext(trans)
 				}
 			}
+		},
+
+		getCity(searchQuery) {
+			const url = `https://ru.wikipedia.org/w/api.php
+						?action=opensearch
+						&format=json
+						&origin=*
+						&limit=10
+						&namespace=0
+						&search=${searchQuery}`
+
+			axios.get(url)
+				.then(data => {
+					this.curCityName = searchQuery
+					this.curCityFact = data.data[2][0]
+				})
+				.catch(err => { throw new Error() })
 		}
 	}
 }
@@ -168,12 +235,7 @@ export default {
 			margin-top: 100px
 			text-transform: capitalize
 			text-align: center
-		&__city-fact
-			padding: 15px   
-			font-size: 16px
-			margin-top: 40px
-			animation: fadeIn .3s ease-in
-		&__start-button
+		&__main-button
 			font-size: 24px
 			text-align: center
 			padding: 10px 15px
@@ -183,7 +245,7 @@ export default {
 			margin: 0 auto
 			outline: none
 			border: none
-			margin-top: 50px
+			margin-top: 30px
 			color: black
 			cursor: pointer
 			box-shadow: 0 1px 4px #e3e4e9
@@ -196,18 +258,24 @@ export default {
 			font-size: 25px
 			border: 1px solid #DC143C
 			border-radius: 5px
-			margin-top: 20px
-		&__stage-one
+			margin: 20px 0
+		&__stage
+			&-one
+				display: flex
+				flex-direction: column
+				align-items: center
+			&-two
+				width: 560px
+		&__transition-wrapper
 			display: flex
 			flex-direction: column
 			align-items: center
-
 		&__main-input
-			margin-top: 50px
+			margin-top: 40px
 			padding: 15px
 			border: 1px solid #e3e4e9
+			width: 90%
 			box-shadow: 0 1px 4px #e3e4e9
-			width: 100%
 			font-size: 24px
 			color: #202020
 			outline: none
@@ -245,6 +313,7 @@ export default {
 		&__current-player
 			margin-top: 100px
 			padding-bottom: 15px
+			width: 50%
 			text-align: center
 			font-size: 32px
 			position: relative
@@ -263,4 +332,46 @@ export default {
 				display: inline-block
 				transform: rotate(45deg)
 
+		&__without-micro
+			& button 
+				outline: none
+				border: none
+				padding: 15px 36px
+				display: block
+				color: #202020
+				margin-top: 15px
+				margin-left: auto
+				cursor: pointer
+				border-radius: 4px
+				font-size: 18px
+				background: none
+				border: 1px solid #e3e4e9
+				&:hover
+					color: white
+					background-color: #202020
+					transition: .3s
+
+		&__city
+			&-name
+				text-align: center
+				margin-top: 50px
+				color: #4D535D
+				font-weight: 500
+			&-fact
+				padding: 15px   
+				font-size: 14px
+				margin-top: 15px
+			&-name,
+			&-fact
+				animation: fadeIn .5s ease-in
+
+			&-count
+				position: absolute
+				left: 40px
+				top: 60px
+				padding: 12px 14px
+				font-size: 22px
+				background-color: #feda6a
+				border-radius: 50%
+				color: #202020
 </style>
